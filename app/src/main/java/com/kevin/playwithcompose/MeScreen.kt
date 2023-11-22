6c055fb69aeb960546aee120fd81948a35fa145b
student9128 <jingpengchn@163.com>
2023-11-22 17:06:01 +0800

    add:me页面添加滚轮列表

--- app/src/main/java/com/kevin/playwithcompose/MeScreen.kt
+++ app/src/main/java/com/kevin/playwithcompose/MeScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,8 +14,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,11 +28,16 @@ import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kevin.playwithcompose.ui.theme.mainColor
+import com.kevin.playwithcompose.ui.theme.onMainColor
 import com.kevin.playwithcompose.ui.theme.primaryLight
 
 @Composable
@@ -69,25 +79,54 @@ fun MeScreen() {
                     .padding(top = 200.dp)
                     .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
                     .background(color = Color.White)
-            ) {
-
-            }
-            Box(
+            )
+            Column(
                 modifier = Modifier
+                    .fillMaxWidth()
                     .align(Alignment.TopCenter)
-                    .padding(top = 150.dp)
-                    .size(100.dp)
-                    .clip(CircleShape)
-                    .border(width = 2.dp, brush = imageBorder, CircleShape)
+                    .padding(top = 150.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_avatar),
-                    contentDescription = "me",
-                    contentScale = ContentScale.Crop,
+                Box(
                     modifier = Modifier
-                        .fillMaxSize()
+                        .size(100.dp)
                         .clip(CircleShape)
+                        .border(width = 2.dp, brush = imageBorder, CircleShape)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_avatar),
+                        contentDescription = "me",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(CircleShape)
+                    )
+                }
+                Text(
+                    text = "一起学习Compose",
+                    color = onMainColor,
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+//                        .background(Color.Green.copy(alpha = 0.5f))
+
                 )
+                LazyColumn(contentPadding = PaddingValues(16.dp), modifier = Modifier.padding(top = 16.dp)) {
+                    items(count = 10) { index ->
+                        Row(Modifier.fillMaxWidth().padding(vertical = 16.dp)) {
+                            Image(
+                                imageVector = Icons.Filled.AccountCircle,
+                                contentDescription = null,
+                                colorFilter = ColorFilter.tint(color = if(index%2==0)Color.Red.copy(alpha = 0.5f) else primaryLight)
+                            )
+                            Text(text = "index = $index")
+                        }
+                    }
+                }
             }
         }
         Text("MeScreen")
